a4a678e64dc76f5e2c29c7ef2d52d703635c4a37
deeonejosh <ogundiyatemidayo@gmail.com>
2025-07-25 02:01:24 +0100

    Add files via upload

---
+++ button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Button({ children }) {
+  return (
+    <button className="bg-blue-600 text-white py-2 px-4 rounded-xl hover:bg-blue-700 transition">
+      {children}
+    </button>
+  );
+}
